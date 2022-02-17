ce52de32b9e2d2fddf4116925190fea1d6a9e72d
mpous <marc@balena.io>
2022-02-17 16:59:18 +0100

    Testing new gatewayrs

--- hm-gatewayrs/start-gatewayrs.sh
+++ hm-gatewayrs/start-gatewayrs.sh
@@ -2,80 +2,66 @@
 
 echo "Starting start-gatewayrs.sh"
 
-TARGET_DIR=/etc/helium_gateway
+rm -f settings.toml
+rm -f /etc/helium_gateway/settings.toml
 
-if [ ! -d ${TARGET_DIR} ] ; then
-  # No directory mounted, better to exit than creating IDs inside a container
-  echo "You must mount a persistent directory for /opt/helium_gateway"
+echo "Checking for I2C device"
+
+mapfile -t data < <(i2cdetect -y 1)
+
+for i in $(seq 1 ${#data[@]}); do
+    # shellcheck disable=SC2206
+    line=(${data[$i]})
+    # shellcheck disable=SC2068
+    if echo ${line[@]:1} | grep -q 60; then
+        echo "ECC is present."
+        ECC_CHIP=True
+    else
+        echo "ECC is not present."
+    fi
+done
+
+echo "Setting REGION_OVERRIDE"
+if [[ -v REGION_OVERRIDE ]]
+then
+  echo "REGION_OVERRED is set to ${REGION_OVERRIDE}"
+  echo 'region = "'"${REGION_OVERRIDE}\"" >> settings.toml
+else
+  echo "REGION_OVERRIDE not set"
   exit 1
 fi
 
-echo "Checking default.toml"
-
-if [ ! -f ${TARGET_DIR}/default.toml ] ; then
-  # The container has not been initialized
-  # move the configuration file to the external directory
-  echo "Initializing gatewayrs"
-  cp -R /etc/helium_gateway/* ${TARGET_DIR}/
-
-  # Add the ZONE in file
-  if [ ! -z "${REGION_OVERRIDE}" ] ; then
-    echo "Setting up Zone for ${REGION_OVERRIDE}"
-    mv ${TARGET_DIR}/settings.toml ${TARGET_DIR}/settings.bak
-    (echo "region=\"${REGION_OVERRIDE}\"" ; cat ${TARGET_DIR}/settings.bak ) > ${TARGET_DIR}/settings.toml
-  fi
-
-  if [ ! -z "${MINER_UPDATE}" ] ; then
-    echo "Setting up auto update status ${MINER_UPDATE}"
-    mv ${TARGET_DIR}/settings.toml ${TARGET_DIR}/settings.bak
-    ( cat ${TARGET_DIR}/settings.bak ; echo "enabled=${MINER_UPDATE}" ) > ${TARGET_DIR}/settings.toml
-  fi 
-  
-  # Check if the gateway_key already exists
-  if [ -f ${TARGET_DIR}/gateway_key.bin ]; then
-    echo "Key file already exists"
-    echo "keypair = '\"${TARGET_DIR}\"/gateway_key.bin'" >> ${TARGET_DIR}/settings.toml
-  else
-      # Change the key path
-      echo "Key file doesn't exist."
-      mv ${TARGET_DIR}/settings.toml ${TARGET_DIR}/settings.bak
-      ( echo "keypair = \"${TARGET_DIR}/gateway_key.bin\"" ; cat ${TARGET_DIR}/settings.bak ) > ${TARGET_DIR}/settings.toml
-      rm ${TARGET_DIR}/settings.bak
-  fi
-
-  # Change the logger
-  mv ${TARGET_DIR}/settings.toml ${TARGET_DIR}/settings.bak
-  cat ${TARGET_DIR}/settings.bak | sed -e 's/syslog/stdio/' > ${TARGET_DIR}/settings.toml
-  rm ${TARGET_DIR}/settings.bak
-
-  # Change the listener
-  mv ${TARGET_DIR}/settings.toml ${TARGET_DIR}/settings.bak
-  cat ${TARGET_DIR}/settings.bak | sed -e 's/listen_addr/listen/' > ${TARGET_DIR}/settings.toml
-  rm ${TARGET_DIR}/settings.bak
-
-  echo "Ready to call helium_gateway server"
-
-  /usr/bin/helium_gateway -c ${TARGET_DIR} server &
-  sleep 3
-
-  # Make the gateway key to be created
-  /usr/bin/helium_gateway -c ${TARGET_DIR} key info
-  GWNAME=`/usr/bin/helium_gateway -c ${TARGET_DIR} key info | grep name | tr -s " " | cut -d ':' -f 2 | sed -e 's/[ \"]//g'` 
-  touch ${TARGET_DIR}/$GWNAME
-
-  # Add the name of the hostpot into the tags
-  #ID=$(curl -sX GET "https://api.balena-cloud.com/v5/device?\$filter=uuid%20eq%20'$BALENA_DEVICE_UUID'" \
-  #-H "Content-Type: application/json" \
-  #-H "Authorization: Bearer $BALENA_API_KEY" | \
-  #jq ".d | .[0] | .id")
-
-  #TAG_NAME=$(curl -sX POST \
-  #"https://api.balena-cloud.com/v5/device_tag" \
-  #-H "Content-Type: application/json" \
-  #-H "Authorization: Bearer $BALENA_API_KEY" \
-  #--data "{ \"device\": \"$ID\", \"tag_key\": \"NAME\", \"value\": \"$GWNAME\" }" > /dev/null)
-
+echo "Interacting with ECC_CHIP"
+if [[ -v ECC_CHIP ]]
+then
+  echo "Using ECC for public key."
+  echo 'keypair = "ecc://i2c-1:96&slot=0"' >> settings.toml
 else
-   echo "Starting helium_gateway server"
-   /usr/bin/helium_gateway -c ${TARGET_DIR} server
-fi 
+  echo "Key file already exists"
+  echo 'keypair = "/var/data/gateway_key.bin"' >> settings.toml
+fi
+
+
+echo "Copying existing settings.toml.template to the new file"
+cat /etc/helium_gateway/settings.toml.template >> settings.toml
+cp settings.toml /etc/helium_gateway/settings.toml
+
+
+#echo "Running keys script"
+#python3 /opt/gatewayrs/keys.py
+
+
+echo "Calling helium_gateway server ..."
+/usr/bin/helium_gateway -c /etc/helium_gateway server
+
+
+echo "Checking key info..."
+
+if ! PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway key info)
+then
+  echo "Can't get miner key info"
+  #exit 1
+else
+  echo "$PUBLIC_KEYS" > /var/data/key_json
+fi
+
